c700e22097003f09fc6b95893953dc6c6d08702e
Ryan Elliott <relliot@skillz.com>
2019-03-29 16:19:30 -0700

    Began reading files

--- src/audio_crossfade.cpp
+++ src/audio_crossfade.cpp
@@ -6,23 +6,40 @@
 //  Copyright © 2019 Ryan Elliott. All rights reserved.
 //
 
+#include <bitset>
+#include <cstdio>
 #include <string>
+#include <vector>
+#include <fstream>
 #include <stdio.h>
 #include <iostream>
-#include <vector>
+#include <string.h>
 
 struct AACFileHeader
 {
 
 };
 
-bool verify_arguments(std::vector<std::string> arguments)
+void verify_arguments(const std::vector<std::string> arguments)
 {
   if (arguments.size() != 3) {
     std::cout << "Invalid number of arguments. ./audio_crossfade [ audio_file1 ] [ audio_file2 ]" << std::endl;
-    return false;
+    exit(EXIT_FAILURE);
   }
-  return true;
+}
+
+void get_file_data(const char *filename, std::vector<char> &file_contents) {
+  FILE *fd = fopen(filename, "r");
+  unsigned char read_byte;
+  int count = 0;
+  while ((char) (read_byte = getc(fd)) != EOF) {
+    #if defined(DEBUG)
+    std::bitset<8> x(read_byte);
+    std::cout << x << " ";
+    std::cout << std::hex << read_byte << std::endl;
+    #endif
+    file_contents.push_back(read_byte);
+  }
 }
 
 int main(int argc, char *argv[])
@@ -32,9 +49,12 @@ int main(int argc, char *argv[])
     arguments.push_back(std::string(argv[i]));
   }
 
-  if (!verify_arguments(arguments)) {
-    return 0;
-  }
+  #if !defined(DEBUG)
+  verify_arguments(arguments);
+  #endif
+
+  std::vector<char> file_one_contents;
+  get_file_data(argv[1], file_one_contents);
 
   return 0;
 }
